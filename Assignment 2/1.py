2724b4118c1275feac0f70e5cb0739f06c2aa632
vxr22100 <122483816+vxr22100@users.noreply.github.com>
2023-01-18 14:45:18 -0600

    Update 1.py

--- Assignment 2/1.py
+++ Assignment 2/1.py
@@ -4,9 +4,9 @@ def fullname(x,y):
 
 #Function that returns every other char in the full_name string
 def string_alternative(z):
-
     return z[::2]
 
+#taking input and assigning to variables
 First_name = input("your first name : ")
 last_name = input("your last name : ")
 
