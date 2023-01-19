137a2c12761a81b6884733e330bb232950c1e713
vxr22100 <122483816+vxr22100@users.noreply.github.com>
2023-01-18 22:37:48 -0600

    Update 3.py

--- Assignment 2/3.py
+++ Assignment 2/3.py
@@ -1,9 +1,16 @@
+#Defining a list
 heightList = []
 
+#taking input from user for number of customers
 noOfCustomers = int(input("Enter number of customers : "))
 
+#for loop to take input and append to list
 for i in range(0, noOfCustomers):
     h = float(input())
-    heightList.append(h*2.54)
-    
-print(heightList)
+    heightList.append(h)
+
+#converting inches to cms and appending to a list
+newlist = [x*2.54 for x in heightList] 
+
+#printing the new list
+print(newlist)
