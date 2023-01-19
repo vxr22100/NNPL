cc49d9cc4c5b6e31af02f6d6a844ca6be60de7c5
vxr22100 <122483816+vxr22100@users.noreply.github.com>
2023-01-18 22:19:07 -0600

    Update 2.py

--- Assignment 2/2.py
+++ Assignment 2/2.py
@@ -1,5 +1,7 @@
+#defining a empty string
 st = ""
 
+#defining a function to count occurances of each word
 def wordcount(st):
     counts = dict()
     w = st.split()
@@ -9,14 +11,17 @@ def wordcount(st):
         else:
             counts[y]=1
     return counts
-
+#reading input.txt file and adding it to st variable
 with open(r'input.txt','r') as file:
     data = file.read()
     st +=data
 
+#calling wordcount function
 a=str(wordcount(st))
 st+="\nWord_Count: \n"
 st+=a
+
+#replacing string to get required output
 st = st.replace("{","")
 st = st.replace("}","")
 st = st.replace("'","")
@@ -24,7 +29,10 @@ st = st.replace("P","\nP")
 st = st.replace(", ","\n")
 print(st)
 
+#opening and writing to the file
 outputfile = open(r'output.txt','w')
 outputfile.write(st)
+
+#closing of txt files
 outputfile.close()
 file.close()
